43bfb8c0947a324de1511cbfdefe4cb713c156ac
frendyadew <frendyadewicaksono@gmail.com>
2024-02-26 01:15:28 +0700

    frendy

---
+++ WorkingWithFeatureClasses - Copy (4)/WorkingWithFeatureClasses.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace WorkingWithFeatureClasses
+{
+    public class WorkingWithFeatureClasses : ESRI.ArcGIS.Desktop.AddIns.Button
+    {
+        frmFeatureClasses formFeatureClasses;
+        public WorkingWithFeatureClasses()
+        {
+        }
+
+        protected override void OnClick()
+        {
+            if (formFeatureClasses == null || formFeatureClasses.IsDisposed)
+                formFeatureClasses = new frmFeatureClasses();
+
+                if (!formFeatureClasses.Visible)
+                    formFeatureClasses.Show(null);
+
+            ArcMap.Application.CurrentTool = null;
+        }
+        protected override void OnUpdate()
+        {
+            Enabled = ArcMap.Application != null;
+        }
+    }
+
+}
---
+++ WorkingWithFeatureClasses - Copy (4)/frmFeatureClasses.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace WorkingWithFeatureClasses
+{
+    partial class frmFeatureClasses
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboLayers = new System.Windows.Forms.ComboBox();
+            this.btnGetValues = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.cboFields = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Layers";
+            // 
+            // cboLayers
+            // 
+            this.cboLayers.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboLayers.FormattingEnabled = true;
+            this.cboLayers.Location = new System.Drawing.Point(116, 42);
+            this.cboLayers.Name = "cboLayers";
+            this.cboLayers.Size = new System.Drawing.Size(417, 24);
+            this.cboLayers.TabIndex = 1;
+            this.cboLayers.SelectedIndexChanged += new System.EventHandler(this.cboLayers_SelectedIndexChanged);
+            // 
+            // btnGetValues
+            // 
+            this.btnGetValues.Location = new System.Drawing.Point(116, 158);
+            this.btnGetValues.Name = "btnGetValues";
+            this.btnGetValues.Size = new System.Drawing.Size(89, 27);
+            this.btnGetValues.TabIndex = 2;
+            this.btnGetValues.Text = "Get Values";
+            this.btnGetValues.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(424, 158);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(89, 27);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // cboFields
+            // 
+            this.cboFields.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboFields.FormattingEnabled = true;
+            this.cboFields.Location = new System.Drawing.Point(116, 92);
+            this.cboFields.Name = "cboFields";
+            this.cboFields.Size = new System.Drawing.Size(417, 24);
+            this.cboFields.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Fields";
+            // 
+            // frmFeatureClasses
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 242);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cboFields);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnGetValues);
+            this.Controls.Add(this.cboLayers);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmFeatureClasses";
+            this.Text = "Working With Feature Classes";
+            this.Load += new System.EventHandler(this.frmFeatureClasses_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboLayers;
+        private System.Windows.Forms.Button btnGetValues;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.ComboBox cboFields;
+        private System.Windows.Forms.Label label2;
+    }
+}
